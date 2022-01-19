2b0ea8102ac935d7864418c727bad4f8a655c2e1
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-19 16:25:40 -0500

    setupInput moved to its own file and required on play.js

--- play.js
+++ play.js
@@ -1,20 +1,9 @@
 const net = require("net");
 const { connect } = require("./client.js")
+const { setupInput } = require("./input.js")
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", (handleUserInput) => {
-    if (handleUserInput === '\u0003') {
-      process.exit();
-    }
-  });
-  return stdin;
-};
+
 
 console.log("Connecting ...");
 connect();
 setupInput();
-
