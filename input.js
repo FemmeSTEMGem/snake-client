ec417c58a057f23df2a2a7a5a88ba6e11a55e19d
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-19 17:12:02 -0500

    setup wasd movement

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 const net = require("net");
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,9 +9,19 @@ const setupInput = function () {
   stdin.on("data", (handleUserInput) => {
     if (handleUserInput === '\u0003') {
       process.exit();
-    }
+    } else if (handleUserInput === "w") {
+      conn.write("Move: up");
+    } else if (handleUserInput === "a") {
+      conn.write("Move: left");
+    } else if (handleUserInput === "s") {
+      conn.write("Move: down");
+    } else if (handleUserInput === "d") {
+      conn.write("Move: right");
+    };
   });
   return stdin;
 };
+
+let connection
 
 module.exports = {setupInput};
