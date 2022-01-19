c919984faa4061718ae927e07bb4ad5f4ef2ab6c
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-19 15:50:17 -0500

    played with movement

--- client.js
+++ client.js
@@ -1,24 +1,25 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
   });
 
   conn.on('connect', () =>{
-    conn.write("Name: SHH")
-    console.log("Connection established!")
+    conn.write("Name: SHH");
+  
+    console.log("Connection established!");
     
-})
+  });
 
   conn.on('data', (data) => {
-    console.log(data)
-  })
+    console.log(data);
+  });
 
   conn.setEncoding("utf8");
 
   return conn;
 };
 
-module.exports = {connect}
+module.exports = {connect};
